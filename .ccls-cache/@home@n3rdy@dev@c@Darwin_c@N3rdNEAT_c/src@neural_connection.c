e03440aa16d6322bd24aa997483fb4ad77e4f64f
aadit-n3rdy <eaadithyaa@gmail.com>
2021-04-18 12:31:58 +0530

    Removed ccls-cache

--- .ccls-cache/@home@n3rdy@dev@c@Darwin_c@N3rdNEAT_c/src@neural_connection.c
+++
@@ -1,9 +0,0 @@
-
-#include "../include/neural_connection.h"
-#include "../include/neural_node.h"
-
-int neural_connection_init(struct neural_connection *connection, struct neural_node *from_node, float weight) {
-	connection->from = from_node;
-	connection->weight = weight;
-	return 0;
-}
